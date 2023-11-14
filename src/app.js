8929528876bfbb28542f1296848f879a26daa41b
phong.mv <phongmv@ycomm.vn>
2023-11-14 12:51:36 +0700

    Section 1: Add folders

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const helmet = require("helmet");
-
+const compression = require('compression')
 //init middlewares
 // app.use(morgan('dev'))
 app.use(morgan('combined'))
@@ -10,13 +10,16 @@ app.use(morgan('combined'))
 // app.use(morgan('short'))
 // app.use(morgan('tiny'))
 app.use(helmet())
+app.use(compression())
 //init db
 
 //init routes
 
 app.get('/', (req, res, next) => {
+  const strCompression = 'Hello Phong'
   return res.status(500).json({
-    message: 'Welcome Jin dev!'
+    message: 'Welcome Jin dev!',
+    metadata: strCompression.repeat(10000)
   })
 })
 
